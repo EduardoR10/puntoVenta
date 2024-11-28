b5b853d7b5cb4a93616d6032f59fbfbc7293fbc2
PaolaRM04 <anapaolaromeromtz@gmail.com>
2024-11-27 20:14:47 -0600

    Cambios auditoria

--- database/migrations/0001_01_01_000000_create_orders_table.php
+++ database/migrations/0001_01_01_000000_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('idemployee')->constrained('employees');
+            $table->foreignId('idustomer')->constrained('customers');
             $table->date('orderdate');
             $table->decimal('subtotal', 8, 2);
             $table->decimal('iva', 8, 2);
